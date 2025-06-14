3b1269af6fe2804815a51b390295f8429114a671
VikaMartynova <vikam1210@gmail.com>
2025-06-14 23:32:59 +0200

    App setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>My App</h1>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
